a546b59f127b6814a013703bbac865143286b9b0
SanthoshKumar <priya-shalini.santhoshkumar@atos.net>
2021-09-23 09:56:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import FirstPage from './Components/FirstPage';
+import Displayweather from './Components/Displayweather';
+import Main from './Components/Main'
+
+function App() {
+  return (
+    <div >
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FirstPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Displayweather from "./Displayweather";
+import { Redirect } from 'react-router-dom'
+import "./FirstPage.css";
+
+function FirstPage() {
+  //const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  const APIKEY = "ea97e52c3dd933f4e06a02012713596a";
+  async function weatherData(e) {
+    e.preventDefault();
+    if (form.city === "") {
+      alert("Add values");
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+     // setWeather({ data: data });
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "city") {
+      setForm({ ...form, city: value });
+    }
+    if (name === "country") {
+      setForm({ ...form, country: value });
+    }
+  };
+  return (
+    <div className="weather">
+      <span className="title">Weather App</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        &nbsp; &nbsp; &nbsp;&nbsp;
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="getweather" onClick={(e) => weatherData(e)}>
+          Submit
+        </button>
+      </form>
+
+     
+    </div>
+  );
+}
+
+export default FirstPage;
+
